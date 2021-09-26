3dc3954f35991dc6697dd018fd55d1d87cbbe9c4
TuanNguyen21 <nguyentuan21012001@gmail.com>
2021-09-26 10:44:07 +0700

    first commit

---
+++ src/main/java/com/sges/SgesApplication.java
@@ -0,0 +1,13 @@
+package com.sges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgesApplication.class, args);
+    }
+
+}
