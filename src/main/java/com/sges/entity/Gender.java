7d5258d1483085d8e1951e241669984958c2c893
TuanNguyen21 <nguyentuan21012001@gmail.com>
2021-09-28 22:50:39 +0700

    C1 - create entity

---
+++ src/main/java/com/sges/entity/Gender.java
@@ -0,0 +1,25 @@
+package com.sges.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Gender implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	private int id;
+	private String name;
+}
